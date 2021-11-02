60fb09b7eb889364efab1fe38a8201199e29588e
sirclo-bachtiar <tio.bachtiar@sirclo.com>
2021-11-02 19:35:43 +0700

    Magento Custom Module Template
    - create custom module template

---
+++ LoginSadisCustomModule/registration.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Custome Module
+ *
+ * @version 1.0.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Icube_CustomModule',
+    __DIR__
+);
